101721256c949b4137fc78f04e241a4c4b9dd4a8
Javey <jiawei23716@sina.com>
2017-12-12 20:38:41 +0800

    upd: readme

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
 # intact-vue
+
+A compatibiliy layer for running Intact[1] component in Vue[2].
+
+## Usage
+
+```js
+import Vue from 'vue';
+import Intact from 'intact-vue';
+
+class IntactComponent extends Intact {
+    get template() {
+        return `<button ev-click={self.onClick.bind(self)}>
+            click {self.get('value')}
+        </button>`;
+    }
+
+    onClick() {
+        this.trigger('click');
+    }
+}
+
+const container = document.createElement('div');
+document.body.appendChild(container);
+const vue = new Vue({
+    el: container,
+    data: {
+        count: 0,
+    },
+    template: `<div>
+        <IntactComponent @click="add" v-model="count"/>
+        <div>count: {{ count }}</div>
+    </div>`,
+    methods: {
+        add() {
+            this.count++;
+        }
+    },
+    components: {IntactComponent}
+});
+```
+
+[1]: http://javey.github.io/intact
+[2]: https://vuejs.org
--- test/index.js
+++ test/index.js
@@ -243,4 +243,37 @@ describe('Unit test', () => {
             });
         });
     });
+
+    describe('Demo', () => {
+        class IntactComponent extends Intact {
+            get template() {
+                return `<button ev-click={self.onClick.bind(self)}>
+                    click {self.get('value')}
+                </button>`;
+            }
+
+            onClick() {
+                this.trigger('click');
+            }
+        }
+
+        const container = document.createElement('div');
+        document.body.appendChild(container);
+        const vue = new Vue({
+            el: container,
+            data: {
+                count: 0,
+            },
+            template: `<div>
+                <IntactComponent @click="add" v-model="count"/>
+                <div>count: {{ count }}</div>
+            </div>`,
+            methods: {
+                add() {
+                    this.count++;
+                }
+            },
+            components: {IntactComponent}
+        });
+    });
 });

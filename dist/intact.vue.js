f075083cb0d029dd83dd3cfd6a441755073c17d0
Javey <jiawei23716@sina.com>
2017-12-14 16:04:39 +0800

    chore: build

--- dist/intact.vue.js
+++ dist/intact.vue.js
@@ -71,62 +71,100 @@ var h = Intact.Vdt.miss.h;
 var patch = Vue.prototype.__patch__;
 
 function normalizeChildren(vNodes) {
-    if (vNodes) {
+    if (Array.isArray(vNodes)) {
         var ret = [];
         vNodes.forEach(function (vNode) {
-            if (isIntactComponent(vNode)) {
-                var options = vNode.componentOptions;
-                vNode = h(options.Ctor, Object.assign({ className: className(vNode) }, vNode.data.attrs), normalizeChildren(options.children), null, vNode.key, vNode.ref);
-            } else {
-                vNode = h(Wrapper, { vueVNode: vNode });
-            }
-            ret.push(vNode);
+            ret.push(normalize(vNode));
         });
         return ret;
     }
     return vNodes;
 }
 
-function attachProps(vNode) {
+function normalize(vNode) {
+    if (isIntactComponent(vNode)) {
+        var options = vNode.componentOptions;
+        return h(options.Ctor, normalizeProps(vNode), null, null, vNode.key, vNode.ref);
+    } else if (vNode.text !== undefined) {
+        return vNode.text;
+    }
+    return h(Wrapper, { vueVNode: vNode }, null, className(vNode));
+}
+
+function normalizeProps(vNode) {
     var componentOptions = vNode.componentOptions;
     var data = vNode.data;
-    var props = Object.assign({
-        // children: normalizeChildren(componentOptions.children)
-    }, data.attrs);
+    var attrs = data.attrs;
+    var propTypes = componentOptions.Ctor.propTypes;
+    var props = {};
+
+    if (attrs) {
+        for (var key in attrs) {
+            if (key === 'staticClass' || key === 'class') continue;
+            var value = attrs[key];
+            if (propTypes && propTypes[key] === Boolean && value === '') {
+                value = true;
+            }
+            props[key] = value;
+        }
+    }
+
+    // add className
+    props.className = className(vNode);
 
     // if exists v-model
     if (data.model) {
         props.value = data.model.value;
     }
 
-    for (var key in componentOptions.listeners) {
+    for (var _key in componentOptions.listeners) {
         // is a v-model directive of vue
-        if (key === 'input') {
+        if (_key === 'input') {
             props['ev-$change:value'] = function (c, v) {
                 componentOptions.listeners.input(v);
             };
         } else {
-            props['ev-' + key] = componentOptions.listeners[key];
+            props['ev-' + _key] = componentOptions.listeners[_key];
         }
     }
 
-    vNode.props = props;
+    // handle children and blocks
+    var slots = resolveSlots(componentOptions.children);
+    Object.assign(props, getChildrenAndBlocks(slots));
 
     return props;
 }
 
-var MockVueComponent = function () {
-    function MockVueComponent() {
-        classCallCheck(this, MockVueComponent);
+function getChildrenAndBlocks(slots) {
+    var d = slots.default,
+        rest = objectWithoutProperties(slots, ['default']);
+
+    var blocks = void 0;
+    if (rest) {
+        blocks = {};
+
+        var _loop = function _loop(key) {
+            blocks[key] = function () {
+                return normalizeChildren(rest[key]);
+            };
+        };
+
+        for (var key in rest) {
+            _loop(key);
+        }
     }
 
-    // mock api
-    MockVueComponent.prototype.$on = function $on() {};
+    return {
+        children: normalizeChildren(d),
+        _blocks: blocks
+    };
+}
 
-    return MockVueComponent;
-}();
-
-MockVueComponent.options = Vue.options;
+// export class MockVueComponent {
+// static options = Vue.options;
+// // mock api
+// $on() {}
+// }
 
 var Wrapper = function () {
     function Wrapper() {
@@ -134,11 +172,11 @@ var Wrapper = function () {
     }
 
     Wrapper.prototype.init = function init(lastVNode, nextVNode) {
-        return patch(null, nextVNode.props.vueVNode);
+        return patch(null, nextVNode.props.vueVNode, false, false, this.parentDom);
     };
 
     Wrapper.prototype.update = function update(lastVNode, nextVNode) {
-        return patch(lastVNode.props.vueVNode, nextVNode.props.vueVNode);
+        return patch(lastVNode.props.vueVNode, nextVNode.props.vueVNode, false, false, this.parentDom);
     };
 
     Wrapper.prototype.destroy = function destroy(vNode) {
@@ -172,6 +210,44 @@ function isIntactComponent(vNode) {
     return i && (i = i.hook) && (i = i.init) && vNode.componentOptions.Ctor.cid === 'IntactVue';
 }
 
+// copy from vue/src/core/instance/render-helpers/resolve-slots.js
+function resolveSlots(children) {
+    var slots = {};
+    if (!children) {
+        return slots;
+    }
+    for (var i = 0, l = children.length; i < l; i++) {
+        var child = children[i];
+        var data = child.data;
+        // remove slot attribute if the node is resolved as a Vue slot node
+        if (data && data.attrs && data.attrs.slot) {
+            delete data.attrs.slot;
+        }
+        if (data && data.slot != null) {
+            var name = data.slot;
+            var slot = slots[name] || (slots[name] = []);
+            if (child.tag === 'template') {
+                slot.push.apply(slot, child.children || []);
+            } else {
+                slot.push(child);
+            }
+        } else {
+            (slots.default || (slots.default = [])).push(child);
+        }
+    }
+    // ignore slots that contains only whitespace
+    for (var _name in slots) {
+        if (slots[_name].every(isWhitespace)) {
+            delete slots[_name];
+        }
+    }
+    return slots;
+}
+
+function isWhitespace(node) {
+    return node.isComment && !node.asyncFactory || node.text === ' ';
+}
+
 // for webpack alias Intact to IntactVue
 var init = Vue.prototype._init;
 
@@ -181,26 +257,20 @@ var IntactVue = function (_Intact) {
     function IntactVue(options) {
         classCallCheck(this, IntactVue);
 
-        var parentVNode = options._parentVnode;
+        var parentVNode = options && options._parentVnode;
         if (parentVNode) {
-
-            // get slots
-            var _this = possibleConstructorReturn(this, _Intact.call(this, attachProps(parentVNode)));
-
-            var vm = new MockVueComponent();
-            init.call(vm, options);
-            _this.$slots = vm.$slots;
-            _this._handleSlots();
-            _this._isVue = vm._isVue;
+            var vNode = normalize(parentVNode);
 
             // inject hook
+            var _this = possibleConstructorReturn(this, _Intact.call(this, vNode.props));
+
             options.mounted = [_this.mount];
 
             _this.$options = options;
             _this.$vnode = parentVNode;
 
-            _this.$lastVNode = parentVNode;
-            _this.parentVNode = parentVNode;
+            _this.parentVNode = vNode;
+            vNode.children = _this;
         } else {
             var _this = possibleConstructorReturn(this, _Intact.call(this, options));
         }
@@ -208,18 +278,20 @@ var IntactVue = function (_Intact) {
     }
 
     IntactVue.prototype.$mount = function $mount(el, hydrating) {
-        this.$el = this.init();
+        this.$el = this.init(null, this.parentVNode);
         this._vnode = {};
         this.$options._parentElm.appendChild(this.$el);
     };
 
     IntactVue.prototype.$forceUpdate = function $forceUpdate() {
         this._initMountedQueue();
-        attachProps(this.$vnode);
-        this._handleSlots();
-        this.update(this.$lastVNode, this.$vnode);
-        this.$lastVNode = this.$vnode;
-        this.parentVNode = this.$vnode;
+
+        var vNode = normalize(this.$vnode);
+        vNode.children = this;
+
+        this.update(this.parentVNode, vNode);
+        this.parentVNode = vNode;
+
         this._triggerMountedQueue();
     };
 
@@ -227,30 +299,12 @@ var IntactVue = function (_Intact) {
         this.destroy();
     };
 
-    IntactVue.prototype._handleSlots = function _handleSlots() {
-        var _$slots = this.$slots,
-            d = _$slots.default,
-            rest = objectWithoutProperties(_$slots, ['default']);
+    // mock api
 
-        var blocks = void 0;
-        if (rest) {
-            blocks = {};
 
-            var _loop = function _loop(key) {
-                blocks[key] = function () {
-                    return normalizeChildren(rest[key]);
-                };
-            };
+    IntactVue.prototype.$on = function $on() {};
 
-            for (var key in rest) {
-                _loop(key);
-            }
-        }
-        this.set({
-            children: normalizeChildren(d),
-            _blocks: blocks
-        }, { update: false });
-    };
+    IntactVue.prototype.$off = function $off() {};
 
     return IntactVue;
 }(Intact);

cf065dfff434190b8e94572c887ce4d381193fef
Javey <jiawei23716@sina.com>
2021-08-24 19:24:48 +0800

    ignore dist/

--- dist/scoped.d.ts
+++
@@ -1,2 +0,0 @@
-import { RendererElement, ComponentInternalInstance, VNode } from 'vue';
-export declare function setScopeId(el: RendererElement, vnode: VNode, scopeId: string | null, slotScopeIds: string[] | null, parentComponent: ComponentInternalInstance | null): void;
